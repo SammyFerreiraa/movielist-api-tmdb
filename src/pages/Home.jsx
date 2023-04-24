452d43ae4eddd4b50e7ccfd5f86d010c4b2abfbf
Sammyferreira1 <sammyfe2021@gmail.com>
2023-04-23 21:12:22 -0300

    search and inforpage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
 
   return (
     <div className="container">
-      <h2 className="title">Melhores filmes:</h2>
+      <h2 className="title">Filmes Populares</h2>
       <div className="movies-container">
         {topMovies.length === 0 && <p>Carregando...</p>}
         {topMovies.length > 0 &&
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill,
+} from "react-icons/bs";
+
+import MovieCard from "../components/MovieCard";
+
+import "../styles/pages/movie.sass";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Movie = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
 
-export default Movie
+  const getMovie = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setMovie(data);
+  };
+
+  useEffect(() => {
+    const movieURL = `${moviesURL}${id}?${apiKey}`;
+    getMovie(movieURL);
+  }, []);
+
+  return (
+    <div className="movie-page">
+      {movie && (
+        <>
+          <MovieCard movie={movie} showLink={false} />
+          <p className="tagline">{movie.tagline}</p>
+          <div className="infor">
+            <h3>
+              <BsWallet2 /> Orçamento:
+            </h3>
+            <p>{movie.revenue}</p>
+          </div>
+
+          <div className="infor">
+            <h3>
+              <BsGraphUp /> Receita:
+            </h3>
+            <p>{movie.revenue} minutos</p>
+          </div>
+
+          <div className="infor">
+            <h3>
+              <BsHourglassSplit /> Duração:
+            </h3>
+            <p>{movie.runtime}</p>
+          </div>
+
+          <div className="infor">
+            <h3>
+              <BsFillFileEarmarkTextFill /> Descrição
+            </h3>
+            <p>{movie.overview}</p>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Movie;
